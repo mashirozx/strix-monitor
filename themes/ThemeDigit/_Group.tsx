239545554c55e240e51cfea31ba37e455c99d511
mashirozx <moezhx@outlook.com>
2023-08-06 14:47:04 +0800

    absolute logo

--- themes/ThemeDigit/_Group.tsx
+++ themes/ThemeDigit/_Group.tsx
@@ -16,15 +16,13 @@ const Group: FunctionComponent<GroupProps> = ({
   logo,
 }) => {
   return (
-    <div className="flex h-full w-1/3 flex-col justify-between rounded-2xl border-8 border-solid border-gray-400 bg-slate-300">
-      <div className="flex flex-row items-start justify-between">
-        <div className="mt-3 flex flex-col gap-6">
-          {normaFactors.map((factor, index) => (
-            <NormalFactor key={index} {...factor} />
-          ))}
-        </div>
-        {logo}
+    <div className="relative flex h-full w-1/3 flex-col justify-between rounded-2xl border-8 border-solid border-gray-400 bg-slate-300">
+      <div className="mt-3 flex flex-col gap-6">
+        {normaFactors.map((factor, index) => (
+          <NormalFactor key={index} {...factor} />
+        ))}
       </div>
+      <div className="absolute right-0 top-0">{logo}</div>
       <div className="flex h-full items-center justify-center">
         <SpecialFactor {...specialFactor} />
       </div>
